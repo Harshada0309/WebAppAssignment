1672ef2adade6c006ca336534a299641cb2c6dc8
Harshada0309 <harshadabhilare12@gmail.com>
2024-10-22 16:12:22 +0530

    Add files via upload

---
+++ Query for crud.sql
@@ -0,0 +1,8 @@
+create database ProductDB 
+CREATE TABLE Products1(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(100) NOT NULL,
+    Description NVARCHAR(255),
+    Price DECIMAL(18,2) NOT NULL,
+    Created DATETIME DEFAULT GETDATE()
+);
